a960e9b6abcd46dfb8e80398062543bd89a8ff20
Laksh Singh <132688007+LakshSingh1@users.noreply.github.com>
2023-07-04 16:05:04 -0700

    Add files via upload

---
+++ For_loops.py
@@ -0,0 +1,23 @@
+for i in range(1, 151):
+    print(i)
+
+
+for i in range(5, 1001, 5):
+    print(i)
+
+
+for i in range(1, 101):
+    if i % 10 == 0:
+        print("Coding Dojo")
+    elif i % 5 == 0:
+        print("Coding")
+    else:
+        print(i)
+
+i = 0
+for i in range(1, 500001, 2):
+    print(i)
+
+
+for i in range(2018, 0, -4):
+    print(i)
